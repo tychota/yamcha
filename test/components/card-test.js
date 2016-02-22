5077c294b8c7de61112e3cb0219134c885f88f47
tychota <tycho.tatitscheff+ananke@gmail.com>
2016-02-22 17:58:43 +0100

    Fixing CI

---
+++ test/components/card-test.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { expect } from 'chai'
+import { shallow } from 'enzyme'
+import Card from '../../src/components/Card.jsx' // eslint-disable-line no-unused-vars
+import { it, describe } from 'mocha'
+
+describe('A suite', () => {
+  it('contains spec with an expectation', () => {
+    expect(shallow(<Card />).contains(<p class="title is-5">John Smith</p>)).to.equal(true)
+  })
+})
